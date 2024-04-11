e9af4fbf3b6e0fd0fc01092250ba46107d696a94
Markus Tacker <Markus.Tacker@NordicSemi.no>
2024-04-11 20:08:53 +0200

    refactor: type layer dependencies
    [skip ci]

--- cdk/baseLayer.ts
+++ cdk/baseLayer.ts
@@ -1,7 +1,12 @@
 import { packLayer, type PackedLayer } from '../src/layer.js'
+import pJson from '../package.json'
+
+const dependencies: Array<
+	keyof (typeof pJson)['devDependencies'] | keyof (typeof pJson)['dependencies']
+> = ['id128']
 
 export const pack = async (): Promise<PackedLayer> =>
 	packLayer({
 		id: 'baseLayer',
-		dependencies: ['id128'],
+		dependencies,
 	})
