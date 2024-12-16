ba6c2e60292e1384835848429d9c14b3dda8cea9
Markus Tacker <m@coderbyheart.com>
2024-12-16 09:07:16 +0100

    feat(logs): set log retention to 30 days for non-test stacks

--- src/LambdaLogGroup.ts
+++ src/LambdaLogGroup.ts
@@ -1,16 +1,22 @@
 import { aws_logs as Logs, Names, RemovalPolicy, Stack } from 'aws-cdk-lib'
 import { Construct } from 'constructs'
+import { isTest } from './isTest.js'
 
 export class LambdaLogGroup extends Construct {
 	public readonly logGroup: Logs.LogGroup
 	constructor(
 		parent: Construct,
 		id: string,
-		retention = Logs.RetentionDays.ONE_DAY,
+		// Defaults to 30 days for production, 1 day for test
+		retention?: Logs.RetentionDays,
 	) {
 		super(parent, id)
 		this.logGroup = new Logs.LogGroup(this, 'logGroup', {
-			retention,
+			retention:
+				retention ??
+				(isTest(this)
+					? Logs.RetentionDays.ONE_DAY
+					: Logs.RetentionDays.ONE_MONTH),
 			logGroupName: `/${Stack.of(this).stackName}/fn/${id}-${Names.uniqueId(this)}`,
 			logGroupClass: Logs.LogGroupClass.STANDARD, // INFREQUENT_ACCESS does not support custom metrics
 			removalPolicy:
